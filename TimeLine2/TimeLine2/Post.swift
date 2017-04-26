9b8ba1782ff3ed7d1935fb74025eca4d3d1db583
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-04-26 16:36:50 -0600

    Added/Implemented Post Controller Manual Sync

--- TimeLine2/TimeLine2/Post.swift
+++ TimeLine2/TimeLine2/Post.swift
@@ -7,8 +7,15 @@
 //
 
 import UIKit
+import CloudKit
 
-class Post {
+class Post: CloudKitSyncable {
+    
+    //MARK: - Keys
+    
+    static let typeKey = "Post"
+    static let photoDataKey = "photoData"
+    static let timestampKey = "timestamp"
     
     //MARK: - Properties
     
@@ -19,6 +26,35 @@ class Post {
         guard let photoData = self.photoData else { return nil }
         let newImage = UIImage(data: photoData)
         return newImage
+    }
+    
+    //MARK: - CloudKitSync
+    convenience required init?(record: CKRecord) {
+        
+        guard let timestamp = record.creationDate,
+            let photoAsset = record[Post.photoDataKey] as? CKAsset else { return nil }
+        
+        let photoData = try? Data(contentsOf: photoAsset.fileURL)
+        self.init(photoData: photoData, timestamp: timestamp)
+        cloudKitRecordID = record.recordID
+    }
+    
+    var recordType: String {
+        return Post.typeKey
+    }
+    var cloudKitRecordID: CKRecordID?
+    
+    fileprivate var temporaryPhotoURL: URL {
+        
+        // Must write to temporary directory to be able to pass image file path url to CKAsset
+        
+        let temporaryDirectory = NSTemporaryDirectory()
+        let temporaryDirectoryURL = URL(fileURLWithPath: temporaryDirectory)
+        let fileURL = temporaryDirectoryURL.appendingPathComponent(UUID().uuidString).appendingPathExtension("jpg")
+        
+        try? photoData?.write(to: fileURL, options: [.atomic])
+        
+        return fileURL
     }
     
     
@@ -36,5 +72,15 @@ extension Post: SearchableRecord {
     func matches(searchTerm: String) -> Bool {
         let matchingComments = comments.filter { $0.matches(searchTerm: searchTerm) }
         return !matchingComments.isEmpty
+    }
+}
+
+extension CKRecord {
+    convenience init(_ post: Post) {
+        let recordID = CKRecordID(recordName: UUID().uuidString)
+        self.init(recordType: post.recordType, recordID: recordID)
+        
+        self[Post.timestampKey] = post.timestamp as CKRecordValue?
+        self[Post.photoDataKey] = CKAsset(fileURL: post.temporaryPhotoURL)
     }
 }
