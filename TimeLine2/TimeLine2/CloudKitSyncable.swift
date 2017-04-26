5f18596be116b62d9ddfbcc6a4337fdb4be2d4ef
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-04-26 16:19:54 -0600

    Implemented Multiple Protocol Extensions for CloudKitSyncable

--- TimeLine2/TimeLine2/CloudKitSyncable.swift
+++ TimeLine2/TimeLine2/CloudKitSyncable.swift
@@ -17,3 +17,22 @@ protocol CloudKitSyncable {
     var recordType: String { get }
     
 }
+
+extension CloudKitSyncable {
+    
+    // helper variable to determine if a CloudKitSyncable has a CKRecordID, which we can use to say that the record has been saved to the server
+    var isSynced: Bool {
+        if cloudKitRecordID != nil {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    // a computed property that returns a CKReference to the object in CloudKit
+    var cloudKitReference: CKReference? {
+        guard let cloudKitRecordID = cloudKitRecordID else { return nil }
+        let CKRef = CKReference(recordID: cloudKitRecordID, action: .none)
+        return CKRef
+    }
+}
