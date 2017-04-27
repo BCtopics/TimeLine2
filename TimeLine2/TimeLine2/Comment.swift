e3aff5ebe18ef6d70b77b82e9571184de5749f55
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-04-26 20:23:13 -0600

    Updated Comment for Sync Functionality

--- TimeLine2/TimeLine2/Comment.swift
+++ TimeLine2/TimeLine2/Comment.swift
@@ -7,9 +7,19 @@
 //
 
 import UIKit
+import CloudKit
 
-class Comment {
+class Comment: CloudKitSyncable {
  
+    
+    //MARK: - Keys
+    
+    static let typeKey = "Comment"
+    static let textKey = "text"
+    static let postKey = "post"
+    static let timestampKey = "timestamp"
+    
+    
     //MARK: - Properties
     
     let text: String
@@ -24,10 +34,39 @@ class Comment {
         self.post = post
     }
     
+    
+    //MARK: - CloudKitSync
+    
+    var cloudKitRecordID: CKRecordID?
+    var recordType: String { return Comment.typeKey }
+    
+    convenience required init?(record: CKRecord) {
+        
+        guard let timestamp = record.creationDate,
+            let text = record[Comment.textKey] as? String else { return nil }
+        
+        self.init(post: nil, text: text, timestamp: timestamp)
+        cloudKitRecordID = record.recordID
+    }
+    
 }
 
 extension Comment: SearchableRecord {
     func matches(searchTerm: String) -> Bool {
         return text.contains(searchTerm)
+    }
+}
+
+extension CKRecord {
+    convenience init(_ comment: Comment) {
+        guard let post = comment.post else { fatalError("Comment does not have a Post relationship") }
+        let postRecordID = post.cloudKitRecordID ?? CKRecord(post).recordID
+        let recordID = CKRecordID(recordName: UUID().uuidString)
+        
+        self.init(recordType: comment.recordType, recordID: recordID)
+        
+        self[Comment.timestampKey] = comment.timestamp as CKRecordValue?
+        self[Comment.textKey] = comment.text as CKRecordValue?
+        self[Comment.postKey] = CKReference(recordID: postRecordID, action: .deleteSelf)
     }
 }
